16b135f958e22496058d6bdf8c36b767c1933084
KaptSprinkles <KSoto18@users.noreply.github.com>
2022-10-30 16:16:02 -0400

    starter in notesAPI

---
+++ routes/notesAPI.js
@@ -0,0 +1,17 @@
+//Modules
+const fs = require('fs');
+
+
+
+
+module.export = function (app){
+   // GET notesAPI & Database
+
+
+   //Post to notes & add to Database
+
+
+   //Write new note into Database
+}
+
+
