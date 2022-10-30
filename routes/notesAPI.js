9e1c1a869bf81d93f82b23ce0b0bdca7255c20fa
KaptSprinkles <KSoto18@users.noreply.github.com>
2022-10-30 17:06:42 -0400

    fixed some bugs

--- routes/notesAPI.js
+++ routes/notesAPI.js
@@ -3,19 +3,19 @@ const fs = require('fs');
 const dataBase = require('../db/db.json');
 
 module.exports = function (app) {
-   // GET Database
-   app.get('/api/notes', (req, res) =>
-     res.json(dataBase)
-   );
+    // GET Database
+    app.get('/api/notes', function (req, res) {
+        res.json(dataBase)
+    });
 
-   //Post notes and add to Database
-   app.post('/api/notes', function (req, res){
-     dataBase.push(req.body);
-  
+    //Post notes and add to Database
+    app.post('/api/notes', function (req, res) {
+        dataBase.push(req.body);
 
-   //Write new note into Database
-   fs.writeFile('../db/db.json', JSON.stringify(dataBase), function () {
-    res.json(dataBase);
-   });
- });
+
+        //Write new note into Database
+        fs.writeFile('../db/db.json', JSON.stringify(dataBase), function () {
+            res.json(dataBase);
+        });
+    });
 }
--- routes/pageRoutes.js
+++ routes/pageRoutes.js
@@ -4,12 +4,12 @@ const path = require('path');
 module.exports = function (app) {
 // GET ROUTE for index (homepage)
 app.get('/', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/index.html'))
+  res.sendFile(path.join(__dirname, '../public/index.html'))
  );
 
 //GET ROUTE for notes
 app.get('/notes', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/notes.html'))
+  res.sendFile(path.join(__dirname, '../public/notes.html'))
  );
 
 }
