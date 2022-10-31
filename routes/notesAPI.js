684e0c6ccd30e53c8e7d341e2d096a4fa46692cd
KaptSprinkles <KSoto18@users.noreply.github.com>
2022-10-31 16:35:35 -0400

    notes can be click and read now, also added console log to see whats going on

--- routes/notesAPI.js
+++ routes/notesAPI.js
@@ -5,23 +5,25 @@ const dataBase = require('../db/db.json');
 module.exports = function (app) {
     // GET Database
     app.get('/api/notes', function (req, res) {
-        res.json(dataBase)
-    });
+        fs.readFile('../db/db.json', 'utf-8', function () {
+            console.info(`Looking for notes...`);
+            res.json(dataBase); 
+        }
+     )});
 
     //Post notes and add to Database
     app.post('/api/notes', function (req, res) {
         dataBase.push(req.body);
        
         // updates the ID for each note
-        forEach((note, i) => {
+        dataBase.forEach((note, i) => {
             note.id = i + 2;
           });
 
         //Write new note into Database
         fs.writeFile('../db/db.json', JSON.stringify(dataBase), function () {
-            console.log(`New note added!`);
             res.json(dataBase);
-            
+            console.info(`New note added!`);
 
         });
     });
